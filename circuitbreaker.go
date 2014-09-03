60a1d21e10bee9d0b5c2aa33e190bc1ff0257c6f
Guillermo Pascual <akira.space@gmail.com>
2014-09-03 16:26:49 +0200

    Add more test cases for the circuit.

--- circuitbreaker.go
+++ circuitbreaker.go
@@ -1,107 +1,105 @@
-package gocircuitbreaker
+package circuitbreaker
 
 import "time"
 
 type Circuit struct {
-	numErrors int
-	commands  chan commandData
-	resetTime time.Duration
+    numErrors int
+    commands  chan commandData
+    resetTime time.Duration
 }
 
 type commandData struct {
-	action int
-	result chan<- interface{}
+    action int
+    result chan<- interface{}
 }
 
 const (
-	OK = iota
-	ERROR
-	GET_NUMBERS_OF_ERRORS
-	RESET
-	IS_RESETTING
-	RESET_FINISHED
+    OK  = iota
+    ERROR
+    GET_NUMBERS_OF_ERRORS
+    RESET
+    IS_RESETTING
+    RESET_FINISHED
 )
 
 func NewCircuit(numErrors int, resetTime time.Duration) *Circuit {
-	circuit := Circuit{
-		numErrors: numErrors,
-		resetTime: resetTime,
-		commands:  make(chan commandData),
-	}
-	go circuit.run()
-	return &circuit
+    circuit := Circuit{
+        numErrors: numErrors,
+        resetTime: resetTime,
+        commands:  make(chan commandData),
+    }
+    go circuit.run()
+    return &circuit
 }
 
 func (crt Circuit) run() {
-	numErrors := 0
-	resetting := false
-	lastError := time.Now()
+    numErrors := 0
+    resetting := false
+    lastError := time.Now()
 
-	for command := range crt.commands {
+    for command := range crt.commands {
+        switch command.action {
+        case GET_NUMBERS_OF_ERRORS:
+            command.result <- numErrors
+        case IS_RESETTING:
+            command.result <- resetting
+        case OK:
+            numErrors = 0
+        case ERROR:
+            now := time.Now()
+            if now.Sub(lastError) >= crt.resetTime {
+                numErrors = 1
+            } else {
+                numErrors++
+            }
+            lastError = now
 
-		switch command.action {
-		case GET_NUMBERS_OF_ERRORS:
-			command.result <- numErrors
-		case IS_RESETTING:
-			command.result <- resetting
-		case OK:
-			numErrors = 0
-		case ERROR:
-			now := time.Now()
-			if now.Sub(lastError) >= crt.resetTime {
-				numErrors = 1
-			} else {
-				numErrors++
-			}
-			lastError = now
-
-			if crt.numErrors == numErrors {
-				resetting = true
-				go crt.waitUntilReset(time.After(crt.resetTime))
-			}
-		case RESET_FINISHED:
-			numErrors = 0
-			resetting = false
-		}
-	}
+            if crt.numErrors == numErrors {
+                resetting = true
+                go crt.waitUntilReset(time.After(crt.resetTime))
+            }
+        case RESET_FINISHED:
+            numErrors = 0
+            resetting = false
+        }
+    }
 }
 
-func (crt Circuit) IsClose() bool {
-	return crt.replyTo(GET_NUMBERS_OF_ERRORS) != crt.numErrors
+func (crt Circuit) IsClosed() bool {
+    return crt.replyTo(GET_NUMBERS_OF_ERRORS) != crt.numErrors
 }
 
 func (crt Circuit) IsOpen() bool {
-	return !crt.IsClose()
+    return !crt.IsClosed()
 }
 
 func (crt Circuit) replyTo(action int) interface{} {
-	reply := make(chan interface{})
-	crt.commands <- commandData{action: action, result: reply}
-	return <-reply
+    reply := make(chan interface{})
+    crt.commands <- commandData{action: action, result: reply}
+    return <-reply
 }
 
 func (crt Circuit) Ok() {
-	crt.newStatus(OK)
+    crt.newStatus(OK)
 }
 
 func (crt Circuit) Error() {
-	crt.newStatus(ERROR)
+    crt.newStatus(ERROR)
 }
 
 func (crt Circuit) newStatus(status int) {
-	if crt.isResetting() {
-		return
-	}
-	crt.commands <- commandData{action: status}
+    if !crt.isResetting() {
+        crt.commands <- commandData{action: status}
+    }
 }
 
 func (crt Circuit) isResetting() bool {
-	return crt.replyTo(IS_RESETTING).(bool)
+    return crt.replyTo(IS_RESETTING).(bool)
 }
 
 func (crt Circuit) waitUntilReset(resetTime <-chan time.Time) {
-	select {
-	case <-resetTime:
-		crt.commands <- commandData{action: RESET_FINISHED}
-	}
+    select {
+    case <-resetTime:
+        crt.commands <- commandData{action: RESET_FINISHED}
+    }
 }
--- circuitbreaker_test.go
+++ circuitbreaker_test.go
@@ -1,24 +1,53 @@
-package gocircuitbreaker
+package circuitbreaker
 
 import (
-	"testing"
-	"time"
+    "testing"
+    "time"
 
-	"github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/assert"
 )
 
-// TODO
-// si mandamos tantos errores como numErrores configurados el circuit IsOpen
-// si mandamos un ok (despues de que este abierto), se cierra IsClosed
-// si esperamos el tiempo de reset despues de IsOpen se cierra IsClosed
-// si mandamos tantos errores como numErrores -1 y esperamos el timpo de reset, aunque enviemos otro
-// error, el circuito sigue cerrado IsClosed
+func newCircuit() *Circuit {
+    return NewCircuit(3, 1*time.Second)
+}
 
-func TestIsOpenWhenItHasTheNumberOfConfiguredErrors(t *testing.T) {
-	assert := assert.New(t)
-	circuit := NewCircuit(3, time.Duration(5)*time.Second)
-	circuit.Error()
-	circuit.Error()
-	circuit.Error()
-	assert.Equal(circuit.IsOpen(), true, "the circuit should be open")
+func TestOpenWhenItHasTheNumberOfConfiguredErrors(t *testing.T) {
+    circuit := newCircuit()
+    circuit.Error()
+    circuit.Error()
+    circuit.Error()
+    assert.Equal(t, circuit.IsOpen(), true)
+}
+
+func TestClosedAfterAnOKWhenItWasOpen(t *testing.T) {
+    circuit := newCircuit()
+    circuit.Error()
+    circuit.Error()
+    circuit.Error()
+    waitUntilReset()
+    circuit.Ok()
+    assert.Equal(t, circuit.IsClosed(), true)
+}
+
+func TestClosedWhenItIsOpenAndResetTimeHasExpired(t *testing.T) {
+    circuit := newCircuit()
+    circuit.Error()
+    circuit.Error()
+    circuit.Error()
+    waitUntilReset()
+    circuit.Ok()
+    assert.Equal(t, circuit.IsClosed(), true)
+}
+
+func TestClosedWhenTheCircuitHasBeenResetAsTimeExpired(t *testing.T) {
+    circuit := newCircuit()
+    circuit.Error()
+    circuit.Error()
+    waitUntilReset()
+    circuit.Error()
+    assert.Equal(t, circuit.IsClosed(), true)
+}
+
+func waitUntilReset() {
+    time.Sleep(1 * time.Second)
 }
