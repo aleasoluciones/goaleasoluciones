5bba189935b63a5c07e70a235f18337e24a42865
Guillermo Pascual <gpascual@alea-soluciones.com>
2014-09-08 14:56:15 +0200

    Refactor assertions

--- circuitbreaker_test.go
+++ circuitbreaker_test.go
@@ -1,53 +1,53 @@
 package circuitbreaker
 
 import (
-    "testing"
-    "time"
+	"testing"
+	"time"
 
-    "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func newCircuit() *Circuit {
-    return NewCircuit(3, 1*time.Second)
+	return NewCircuit(3, 1*time.Second)
 }
 
 func TestOpenWhenItHasTheNumberOfConfiguredErrors(t *testing.T) {
-    circuit := newCircuit()
-    circuit.Error()
-    circuit.Error()
-    circuit.Error()
-    assert.Equal(t, circuit.IsOpen(), true)
+	circuit := newCircuit()
+	circuit.Error()
+	circuit.Error()
+	circuit.Error()
+	assert.True(t, circuit.IsOpen())
 }
 
 func TestClosedAfterAnOKWhenItWasOpen(t *testing.T) {
-    circuit := newCircuit()
-    circuit.Error()
-    circuit.Error()
-    circuit.Error()
-    waitUntilReset()
-    circuit.Ok()
-    assert.Equal(t, circuit.IsClosed(), true)
+	circuit := newCircuit()
+	circuit.Error()
+	circuit.Error()
+	circuit.Error()
+	waitUntilReset()
+	circuit.Ok()
+	assert.True(t, circuit.IsClosed())
 }
 
 func TestClosedWhenItIsOpenAndResetTimeHasExpired(t *testing.T) {
-    circuit := newCircuit()
-    circuit.Error()
-    circuit.Error()
-    circuit.Error()
-    waitUntilReset()
-    circuit.Ok()
-    assert.Equal(t, circuit.IsClosed(), true)
+	circuit := newCircuit()
+	circuit.Error()
+	circuit.Error()
+	circuit.Error()
+	waitUntilReset()
+	circuit.Ok()
+	assert.True(t, circuit.IsClosed())
 }
 
 func TestClosedWhenTheCircuitHasBeenResetAsTimeExpired(t *testing.T) {
-    circuit := newCircuit()
-    circuit.Error()
-    circuit.Error()
-    waitUntilReset()
-    circuit.Error()
-    assert.Equal(t, circuit.IsClosed(), true)
+	circuit := newCircuit()
+	circuit.Error()
+	circuit.Error()
+	waitUntilReset()
+	circuit.Error()
+	assert.True(t, circuit.IsClosed())
 }
 
 func waitUntilReset() {
-    time.Sleep(1 * time.Second)
+	time.Sleep(1 * time.Second)
 }
