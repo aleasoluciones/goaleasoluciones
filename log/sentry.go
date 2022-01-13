aa5838a6364ec53215fcb61e44752ffacaafd9f6
javikalsan <jcalzado@alea-soluciones.com>
2022-01-13 12:22:39 +0100

    [FEATURE][INTERNAL][WIP] Handle panics
    - Update sentry-sdk
    - Paralell change:
        - Add new initialization
        - Mark old sdk code as deprecated
        - Add function to handle panics

--- log/sentry.go
+++ log/sentry.go
@@ -1,13 +1,18 @@
 package log
 
 import (
-	"github.com/getsentry/raven-go"
 	"log"
 	"os"
+	"time"
+
+	"github.com/getsentry/raven-go"
+	"github.com/getsentry/sentry-go"
 )
 
+// isSentryActive initialization was turned to false to disable sentry
 var isSentryActive bool = false
 
+// deprecated
 func InitializeSentry() {
 	sentry := os.Getenv("SENTRY_DSN")
 	if sentry == "" {
@@ -17,6 +22,26 @@ func InitializeSentry() {
 	raven.SetDSN(sentry)
 }
 
+func InitSentry() {
+	sentry_dsn := os.Getenv("SENTRY_DSN")
+	err := sentry.Init(sentry.ClientOptions{
+		Dsn: sentry_dsn,
+	})
+	if err != nil {
+		log.Fatalf("sentry.Init: %s", err)
+		panic("Can't start sentry")
+	}
+}
+
+func HandlePanic() {
+	err := recover()
+	if err != nil {
+		sentry.CurrentHub().Recover(err)
+		sentry.Flush(time.Second * 5)
+	}
+}
+
+// deprecated
 func LogError2Sentry(err error) {
 	if isSentryActive {
 		raven.CaptureError(err, nil)
